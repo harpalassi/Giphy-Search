b9022446f11b34256395c0b368e528f353bb7190
Speedway <harpalassi@gmail.com>
2018-11-04 16:28:34 -0600

    initial gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 // variables
 var topics = ["The Sopranos", "Shameless", "Degrassi", "Alan Partridge", "The IT Crowd"];
+var tvShow = "";
+var queryURL = "";
 
 //loop to create buttons on html page
 for (var i = 0; i < topics.length; i++) {
@@ -8,5 +10,34 @@ for (var i = 0; i < topics.length; i++) {
     newBtn.attr("type", "button");
     newBtn.text(topics[i]);
     $("#buttonarea").append(newBtn);
-
 }
+
+$("button").on("click", function() {
+    tvShow = (this.textContent);
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        tvShow + "&api_key=Wc14oX9Ync7H65rBWNbS7ZRMYIOJSGq7&limit=10";
+
+ $.ajax({url: queryURL, method: "GET"})
+ .then(function(response) {
+     var results = response.data;
+     console.log(results);
+
+     for (var i = 0; i < results.length; i++) {
+        var gifDiv = $("<div>");
+        gifDiv.addClass("gif-block")
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var gif = $("<img>");
+        gif.attr("src", results[i].images.fixed_height.url);
+        gif.addClass("img-responsive")
+        gifDiv.prepend(p);
+        gifDiv.prepend(gif);
+        $("#gifs-here").prepend(gifDiv);
+      }
+
+
+
+
+})
+
+})
