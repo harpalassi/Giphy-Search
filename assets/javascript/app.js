64b0439796c2cfe45086cc60acbe6a619e7aee7e
Speedway <harpalassi@gmail.com>
2018-11-04 21:45:46 -0600

    updated placeholder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 // variables
-var topics = ["The Sopranos", "Shameless", "Degrassi", "Curb Your Enthusiasm", "The IT Crowd", "Saved By The Bell"];
+var topics = ["The Sopranos", "Shameless", "Twin Peaks", "Curb Your Enthusiasm", "The IT Crowd", "Saved By The Bell"];
 var tvShow = "";
 var queryURL = "";
 
