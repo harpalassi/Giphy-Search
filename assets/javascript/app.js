23028f101b46f8ba9e52abba3428f3488996fe5c
Speedway <harpalassi@gmail.com>
2018-11-04 22:07:23 -0600

    cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,37 +15,37 @@ function renderButtons() {
         newBtn.attr("type", "button");
         newBtn.text(topics[i]);
         $("#buttonarea").append(newBtn);
-    }
+    };
 
 $(".tvButton").on("click", function() {
-$(".gifs-here").empty();
-tvShow = (this.textContent);
-queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+    $(".gifs-here").empty();
+    tvShow = (this.textContent);
+    queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
     tvShow + "&api_key=Wc14oX9Ync7H65rBWNbS7ZRMYIOJSGq7&limit=10";
 
 $.ajax({url: queryURL, method: "GET"})
-.then(function(response) {
-    var results = response.data;
-    console.log(results);
+    .then(function(response) {
+        var results = response.data;
+        console.log(results);
 
     for (var i = 0; i < results.length; i++) {
-    var gifDiv = $("<div>");
-    gifDiv.addClass("gif-block col-3-sm")
-    var rating = results[i].rating;
-    var p = $("<p>").text("Rated: " + rating.toUpperCase());
-    var gif = $("<img>");
-    gif.attr("src", results[i].images.original_still.url);
-    gif.attr("data-still", results[i].images.original_still.url)
-    gif.attr("data-animate", results[i].images.original.url)
-    gif.attr("data-state", "still");
-    gif.addClass("img-responsive center-block gif")
-    gifDiv.prepend(p);
-    gifDiv.prepend(gif);
-    $(".gifs-here").prepend(gifDiv);   
+        var gifDiv = $("<div>");
+        gifDiv.addClass("gif-block col-3-sm")
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rated: " + rating.toUpperCase());
+        var gif = $("<img>");
+        gif.attr("src", results[i].images.original_still.url);
+        gif.attr("data-still", results[i].images.original_still.url)
+        gif.attr("data-animate", results[i].images.original.url)
+        gif.attr("data-state", "still");
+        gif.addClass("img-responsive center-block gif")
+        gifDiv.prepend(p);
+        gifDiv.prepend(gif);
+        $(".gifs-here").prepend(gifDiv);   
     }
 
     $(".gif").on("click", function() {
-    var state = $(this).attr("data-state");
+        var state = $(this).attr("data-state");
     if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
@@ -54,7 +54,6 @@ $.ajax({url: queryURL, method: "GET"})
         $(this).attr("data-state", "still");
         }});
  })
-
  })
 };
 
