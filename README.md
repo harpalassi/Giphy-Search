be8f6ec9cdc70c77e396954c035947b730a2f171
Speedway <harpalassi@gmail.com>
2019-03-25 16:44:32 -0500

    updated comments

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # The Ultimate TV Show GIF Page 📺
 
-Input your favorite TV Show and retrieve all the GIFs you could ever want!
+Input your favorite TV Show and retrieve all the GIFs you could ever want! I utilized JavaScript, jQuery, HTML5 and CSS3 to make AJAX requests to the Giphy API and render results to the page.
 
 ![screenshot](assets/images/giphy-search.png)
 
 ## Functionality
+
 User may click pre-selected buttons to view GIFs from each TV Show, or may enter any search term in the text field to add a new show. Please click on a GIF that you would like to animate.
 
-🚀 Deployed at: http://harpalassi.github.io/Giphy-Search
+🚀 Deployed at: http://harpalassi.github.io/tv-show-gifs
 
 ## Built With
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,67 +1,76 @@
-// variables
-
-var topics = ["The Sopranos", "Peep Show", "Alan Partridge", "Twin Peaks", "Curb Your Enthusiasm", "The IT Crowd", "Saved By The Bell"];
-var tvShow = "";
-var queryURL = "";
-
+// data needed to populate page
+const topics = [
+  'The Sopranos',
+  'Peep Show',
+  'Alan Partridge',
+  'Twin Peaks',
+  'Curb Your Enthusiasm',
+  'The IT Crowd',
+  'Saved By The Bell'
+];
+let tvShow = '';
+let queryURL = '';
 
 //loop to create buttons on html page
+const renderButtons = () => {
+  $('#buttonarea').empty();
+  for (let i = 0; i < topics.length; i++) {
+    let newBtn = $('<button>');
+    newBtn.addClass('btn btn-primary btn-sm ml-2 mb-1 mt-3 tvButton');
+    newBtn.attr('type', 'button');
+    newBtn.text(topics[i]);
+    $('#buttonarea').append(newBtn);
+  }
 
-function renderButtons() {
-    $("#buttonarea").empty();
-    for (var i = 0; i < topics.length; i++) {
-        var newBtn = $("<button>");
-        newBtn.addClass("btn btn-primary btn-sm ml-2 mb-1 mt-3 tvButton");
-        newBtn.attr("type", "button");
-        newBtn.text(topics[i]);
-        $("#buttonarea").append(newBtn);
-    };
-
-$(".tvButton").on("click", function() {
-    $(".gifs-here").empty();
-    tvShow = (this.textContent);
-    queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    tvShow + "&api_key=Wc14oX9Ync7H65rBWNbS7ZRMYIOJSGq7&limit=10";
-
-$.ajax({url: queryURL, method: "GET"})
-    .then(function(response) {
-        var results = response.data;
-        console.log(results);
-
-    for (var i = 0; i < results.length; i++) {
-        var gifDiv = $("<div>");
-        gifDiv.addClass("gif-block col-3-sm")
-        var rating = results[i].rating;
-        var p = $("<p>").text("Rated: " + rating.toUpperCase());
-        var gif = $("<img>");
-        gif.attr("src", results[i].images.original_still.url);
-        gif.attr("data-still", results[i].images.original_still.url)
-        gif.attr("data-animate", results[i].images.original.url)
-        gif.attr("data-state", "still");
-        gif.addClass("img-responsive center-block gif")
+  //query the results for the button clicked
+  $('.tvButton').on('click', function() {
+    $('.gifs-here').empty();
+    tvShow = this.textContent;
+    queryURL =
+      'https://api.giphy.com/v1/gifs/search?q=' +
+      tvShow +
+      '&api_key=Wc14oX9Ync7H65rBWNbS7ZRMYIOJSGq7&limit=10';
+    //send ajax request
+    $.ajax({ url: queryURL, method: 'GET' }).then(function(response) {
+      let results = response.data;
+      console.log(results);
+      //print results to the DOM
+      for (let i = 0; i < results.length; i++) {
+        let gifDiv = $('<div>');
+        gifDiv.addClass('gif-block col-3-sm');
+        let rating = results[i].rating;
+        let p = $('<p>').text('Rated: ' + rating.toUpperCase());
+        let gif = $('<img>');
+        gif.attr('src', results[i].images.original_still.url);
+        gif.attr('data-still', results[i].images.original_still.url);
+        gif.attr('data-animate', results[i].images.original.url);
+        gif.attr('data-state', 'still');
+        gif.addClass('img-responsive center-block gif');
         gifDiv.prepend(p);
         gifDiv.prepend(gif);
-        $(".gifs-here").prepend(gifDiv);   
-    }
+        $('.gifs-here').prepend(gifDiv);
+      }
 
-    $(".gif").on("click", function() {
-        var state = $(this).attr("data-state");
-    if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-        } else if (state === "animate") {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-        }});
- })
- })
+      //animate or pause the gif on click
+      $('.gif').on('click', function() {
+        let state = $(this).attr('data-state');
+        if (state === 'still') {
+          $(this).attr('src', $(this).attr('data-animate'));
+          $(this).attr('data-state', 'animate');
+        } else if (state === 'animate') {
+          $(this).attr('src', $(this).attr('data-still'));
+          $(this).attr('data-state', 'still');
+        }
+      });
+    });
+  });
 };
 
 renderButtons();
 
-$("#addTvShow").on("click", function(event) {
-    event.preventDefault();
-    topics.push($("#tv-input").val());
-    renderButtons(); 
+//adds new tv show
+$('#addTvShow').on('click', function(event) {
+  event.preventDefault();
+  topics.push($('#tv-input').val());
+  renderButtons();
 });
-
